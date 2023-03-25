5b333c93ae4164cabb2be53009d64ac83a3cd88c
gglt <2928569881@qq.com>
2023-03-25 18:35:01 +0800

    commit

--- app/src/main/java/pers/gglt/note/communication/AsyncTask_.java
+++ app/src/main/java/pers/gglt/note/communication/AsyncTask_.java
@@ -1,10 +1,22 @@
 package pers.gglt.note.communication;
 
-public class AsyncTask_ {
+import android.os.AsyncTask;
+
+public class AsyncTask_ extends AsyncTask {
+
 
     /**原理*/
     // 有两个线程池，一个用于任务排队，一个用于执行任务
     // 有一个 Handler，用于将执行环境从线程池切换到主线程
 
 
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+    protected void onProgressUpdate(Object[] values) {
+        super.onProgressUpdate(values);
+    }
 }
--- app/src/main/java/pers/gglt/note/widget/Keyboard.java
+++ app/src/main/java/pers/gglt/note/widget/Keyboard.java
@@ -1,11 +1,13 @@
 package pers.gglt.note.widget;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.LinearLayout;
 
-public class Keyboard {
+public class Keyboard extends Activity {
     /**禁止 EditText 自动弹出*/
     void forbidPopup() {
         // 在布局文件的 <EditText> 中 (取消自动获取焦点)
@@ -17,8 +19,18 @@ public class Keyboard {
         // getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
+    /**隐藏*/
     void hide(Context context, View view) {
         InputMethodManager manager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
         manager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    /**监听*/
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_0:
+            case KeyEvent.KEYCODE_1:
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
