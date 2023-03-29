a69a3b4a4da39164bf3ae9e36f81c8cae3953336
gglt <2928569881@qq.com>
2023-03-30 01:06:16 +0800

    commit

--- app/src/main/java/pers/gglt/note/framework/mvc/MVC.java
+++ app/src/main/java/pers/gglt/note/framework/mvc/MVC.java
@@ -1,4 +1,15 @@
 package pers.gglt.note.framework.mvc;
 
 public class MVC {
+    /**概念*/
+    // M (处理数据逻辑) (通知V更新数据)
+    // V (展示数据)
+    // C (M和V间数据传递)
+
+    /**流程*/
+    // 用户事件通知V,再通知到C,再通知到M，M处理完后通知V更新
+
+    /**缺点*/
+    // 耦合 (V改动会导致M和C改动) (Activity/Fragment同时负责V和C)
+    // View直接从Model读取数据
 }
