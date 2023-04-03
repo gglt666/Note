c99b086b1649e38acff717f7dd7c79c041e83a11
gglt <2928569881@qq.com>
2023-04-03 08:58:03 +0800

    commit

--- project/src/test/java/pers/gglt/project/UnitTest.java
+++ project/src/test/java/pers/gglt/project/UnitTest.java
@@ -9,11 +9,6 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class UnitTest {
 
     @Test
