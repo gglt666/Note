0d730c9f76433d009598a4cc2a47884abb332df3
gglt <2928569881@qq.com>
2023-04-08 00:19:23 +0800

    commit

--- note/src/main/java/pers/gglt/note/network/http/Https.java
+++ note/src/main/java/pers/gglt/note/network/http/Https.java
@@ -3,4 +3,10 @@ package pers.gglt.note.network.http;
 public class Https {
     // Https多了一层证书机制，证书相关的API如X509Certificate、KeyStore、SSLSocketFactory等
     // 若忽视证书/域名的校验，则默认访问的https网页都是安全通过的 (失去意义)
+
+    /**概念*/
+    // 在HTTP和TCP中间增加TLS/SSL加密层,对HTTP的数据加密后再给TCP进行传输
+
+    /**加密流程*/
+
 }
