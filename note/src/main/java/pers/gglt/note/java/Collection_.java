2d0081c654cff32db616cb919329c24544195531
gglt <2928569881@qq.com>
2023-04-24 20:01:13 +0800

    Company

---
+++ note/src/main/java/pers/gglt/note/java/Collection_.java
@@ -0,0 +1,27 @@
+package pers.gglt.note.java;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Collection_ {
+    ArrayList list = new ArrayList();
+
+
+    /**
+     * 遍历
+     */
+    void for_() {
+        for (int i = 0; i < list.size(); i++) ;
+    }
+
+    void enhancedFor() {
+        for (Object o : list) ;
+    }
+
+    void iterator() {
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Object o = iterator.next();
+        }
+    }
+}
