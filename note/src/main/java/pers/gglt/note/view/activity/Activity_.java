2ed9893bf26f999d2ae9048a1b4a34d9d5aca414
gglt <2928569881@qq.com>
2023-04-25 08:49:38 +0800

    Dell

--- note/src/main/java/pers/gglt/note/view/activity/Activity_.java
+++ note/src/main/java/pers/gglt/note/view/activity/Activity_.java
@@ -5,4 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Activity_ extends AppCompatActivity {
 
     //https://zhuanlan.zhihu.com/p/449585533
+
+    /**生命周期*/
+    // 横竖屏切换
+    // 要看是否设置了android:configChanges
 }
