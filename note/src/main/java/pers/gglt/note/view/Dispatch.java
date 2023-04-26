7bf747f0b0d9d7a58f0e851e3f70159303f434d4
gglt <2928569881@qq.com>
2023-04-26 20:29:44 +0800

    Company

--- note/src/main/java/pers/gglt/note/view/Dispatch.java
+++ note/src/main/java/pers/gglt/note/view/Dispatch.java
@@ -2,5 +2,8 @@ package pers.gglt.note.view;
 
 public class Dispatch {
 
-    //离用户触摸点最近的控件响应触摸事件,若没有实现响应事件,则向父类传递事件,
+    /**流程*/
+    //离用户触摸点最近的控件响应触摸事件,若没有实现响应事件,则向父类传递事件,有view响应时,会将事件流传递给view中的onTouchEvent()
+
+    //https://cloud.tencent.com/developer/article/1770258?areaSource=103001.22&traceId=5-e5qS9hH4qylkNs4DdNm
 }
